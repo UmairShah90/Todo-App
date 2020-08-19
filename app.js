bcd14b13195b0a9737584c629b517d30364196d0
umair <umair.shah1817@gmail.com>
2020-08-19 14:50:43 +0500

    Todo App Using firebase

--- app.js
+++ app.js
@@ -3,20 +3,20 @@
 
   var list = document.getElementById("list");
 
-function addTodo(){
-    var todoItem = document.getElementById("todo-item");
+  firebase.database().ref('todos').on('child_added', function(data){
 
     // create list item with Text node
     var li = document.createElement('li');
-    var liText = document.createTextNode(todoItem.value)
+    var liText = document.createTextNode(data.val().value);
     li.appendChild(liText);
 
-    todoItem.value = "";
+   
 
-   // create delete button 
+     // create delete button 
    var delbtn = document.createElement("button");
    var delbtnText = document.createTextNode("Delete");
    delbtn.setAttribute("class","deletebtn")
+   delbtn.setAttribute("id",data.val().key)
    delbtn.setAttribute("onClick","deleteItem(this)")
    delbtn.appendChild(delbtnText); 
    
@@ -25,6 +25,7 @@ function addTodo(){
    var editbtn = document.createElement("button");
    var editbtnText = document.createTextNode("Edit");
    editbtn.setAttribute("class","editbtn");
+   editbtn.setAttribute("id",data.val().key)
    editbtn.setAttribute("onClick","editButton(this)");
    editbtn.appendChild(editbtnText);
 
@@ -34,21 +35,45 @@ function addTodo(){
 
 
     list.appendChild(li);
-    console.log(li)
+    
+  })
+
+  
+
+function addTodo(){
+    var todoItem = document.getElementById("todo-item");
+    var database = firebase.database().ref('todos');
+
+    var key = database.push().key
+
+    var todo = {
+        value: todoItem.value,
+        key: key
+    }
+    database.child(key).set(todo)
+     todoItem.value = "";
     
 }
 
+
 function deleteItem(e){
+    firebase.database().ref('todos').child(e.id).remove();
     e.parentNode.remove();
 }
 function deleteAll(){
+    firebase.database().ref('todos').remove();
     list.innerHTML = "";
 
 }
 
 function editButton(e){
-   var val =  e.parentNode.firstChild.nodeValue;
-   var editValue = prompt("Enter new task",val)
+   
+   var val = prompt("Enter new task",e.parentNode.firstChild.nodeValue)
+   var editTodo = {
+       value: val,
+       key: e.id
 
-   e.parentNode.firstChild.nodeValue = editValue;
+   }
+   firebase.database().ref('todos').child(e.id).set(editTodo)
+   e.parentNode.firstChild.nodeValue = val;
 } 
